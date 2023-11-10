4caf18de6ccd6ff0e4d54c395540edea5b862bf3
alysson <alysson18.af@gmail.com>
2023-11-10 09:45:00 -0300

    scalability/redis-ioadapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupRedisAdapter } from './redis/redis-adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  try {
+    const redisAdapter = await setupRedisAdapter();
+    const server = app.getHttpServer();
+    const io = server.get('_io');
+    if (io) {
+      io.adapter(redisAdapter);
+      console.log('Redis adapter initialized');
+    }
+  } catch (error) {
+    console.warn('Redis adapter not available, using in-memory adapter');
+  }
 
   await app.listen(3000);
   console.log('Application is running on http://localhost:3000');
---
+++ src/redis/redis-adapter.ts
@@ -0,0 +1,16 @@
+import { createAdapter } from 'socket.io-redis';
+import { createClient } from 'redis';
+
+export async function setupRedisAdapter() {
+  const pubClient = createClient({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT || '6379'),
+  });
+
+  const subClient = pubClient.duplicate();
+
+  await pubClient.connect();
+  await subClient.connect();
+
+  return createAdapter(pubClient, subClient);
+}
